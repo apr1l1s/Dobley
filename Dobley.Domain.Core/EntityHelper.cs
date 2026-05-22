ca9ede1fb24f0ed8200b50b30b2bb957c71d4f98
Дрейн Александр Дмитриевич <a.safronov@teledoc.team>
2026-05-22 17:44:18 +0300

    ref: добавил хелпер для работы с сущностями

--- Dobley.Domain.Core/EntityHelper.cs
+++ Dobley.Domain.Core/EntityHelper.cs
@@ -4,5 +4,7 @@ namespace Dobley.Domain.Core;
 
 public static class EntityHelper
 {
-    
+    public static bool IsNullOrEmpty(this string? value) => value == null || string.IsNullOrEmpty(value);
+
+    public static bool IsNullOrEmpty<TEntity>(this IEnumerable<TEntity>? value) => value == null || !value.Any();
 }
